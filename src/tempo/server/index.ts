37f3bdf2d37e9fd5616a4a9f64e1cde722bba6cf
slokh <slokhx@gmail.com>
2026-02-09 21:13:23 -0500

    feat: export  for streams

--- src/tempo/server/index.ts
+++ src/tempo/server/index.ts
@@ -1,3 +1,3 @@
 export type { ChannelState, ChannelStorage, SessionState } from '../stream/Storage.js'
 export { charge } from './Charge.js'
-export { stream } from './Stream.js'
+export { settle, stream } from './Stream.js'
