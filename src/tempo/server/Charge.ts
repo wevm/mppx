607815309c68769f243088d959f924d8ae2ed154
jxom <7336481+jxom@users.noreply.github.com>
2026-02-11 10:25:29 +1100

    chore: fmt

--- src/tempo/server/Charge.ts
+++ src/tempo/server/Charge.ts
@@ -263,21 +263,8 @@ export declare namespace charge {
   type Parameters = {
     /** Testnet mode. */
     testnet?: boolean | undefined
-  } & (
-    | {
-        /** Recipient account. Address is used as the payment recipient. */
-        recipient?: Account | undefined
-        /** When true, the recipient account also sponsors (pays) transaction fees. */
-        feePayer?: true | undefined
-      }
-    | {
-        /** Address that receives payment. */
-        recipient?: string | undefined
-        /** Optional fee payer account for covering transaction fees. */
-        feePayer?: Account | undefined
-      }
-  ) &
-    Client.getResolver.Parameters &
+  } & Client.getResolver.Parameters &
+    types.FeePayerParameters &
     Defaults
 
   type DeriveDefaults<parameters extends Parameters> = types.DeriveDefaults<
--- src/tempo/server/Stream.ts
+++ src/tempo/server/Stream.ts
@@ -212,21 +212,8 @@ export declare namespace stream {
     minVoucherDelta?: string | undefined
     /** Testnet mode. */
     testnet?: boolean | undefined
-  } & (
-    | {
-        /** Recipient account. Address is used as the payment recipient. */
-        recipient?: Account | undefined
-        /** When true, the recipient account also sponsors (pays) transaction fees. */
-        feePayer?: true | undefined
-      }
-    | {
-        /** Address that receives payment. */
-        recipient?: string | undefined
-        /** Optional fee payer account for covering open/topUp transaction fees. */
-        feePayer?: Account | undefined
-      }
-  ) &
-    Client.getResolver.Parameters &
+  } & Client.getResolver.Parameters &
+    types.FeePayerParameters &
     Defaults
 
   type DeriveDefaults<parameters extends Parameters> = types.DeriveDefaults<
